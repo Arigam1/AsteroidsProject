961557f1470ef3e6eee32916224b511e113e3590
Arigam1 <yebkobvsebolshe@gmail.com>
2021-04-15 22:35:23 +0400

    end,,

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AsteroidProvider } from "./providers/AsteroidProvider";
 import { DestroyProvider } from "./providers/DestroyProvider";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <AsteroidProvider>
       <DestroyProvider>
         <App />
       </DestroyProvider>
     </AsteroidProvider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
