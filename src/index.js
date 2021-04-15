f4438c811a5da040afc41c652101fa96717dec90
Arigam1 <yebkobvsebolshe@gmail.com>
2021-04-15 22:28:45 +0400

    hash for deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AsteroidProvider } from "./providers/AsteroidProvider";
 import { DestroyProvider } from "./providers/DestroyProvider";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <AsteroidProvider>
       <DestroyProvider>
         <App />
       </DestroyProvider>
     </AsteroidProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
